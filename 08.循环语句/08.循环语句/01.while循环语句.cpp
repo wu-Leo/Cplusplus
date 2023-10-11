08ffb7a957cf0d39d01b4c4dc8bd6e102ea6e792
wu-Leo <1910821966@qq.com>
2023-10-11 20:06:33 +0800

    test

---
+++ 08.循环语句/08.循环语句/01.while循环语句.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+#include <windows.h>
+using namespace std;
+
+int main(int argc,char *argv[]){
+
+	cout << "hello world" << endl;
+
+
+	system("pause");
+	return 0;
+}
