5df5dcdf78d80d62dcb5cd3b9083c22fed6066b8
wu-Leo <121737686+wu-Leo@users.noreply.github.com>
2023-10-12 12:25:25 +0800

    Update 01.while循环语句.cpp

--- 08.循环语句/08.循环语句/01.while循环语句.cpp
+++ 08.循环语句/08.循环语句/01.while循环语句.cpp
@@ -4,10 +4,18 @@
 #include <windows.h>
 using namespace std;
 
+/* 1. while 循环语句 */
 int main(int argc,char *argv[]){
 
-	cout << "hello world" << endl;
+	int i = 1;
+	int sum = 0;
 
+	while (i <= 100) {
+		sum += i;
+		i++;
+	}
+	
+	cout <<"sum="<< sum << endl;
 
 	system("pause");
 	return 0;
